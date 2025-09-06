ea060dae70e2b43509c419a3dad3bc607318b4de
Md. Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-09-06 23:37:51 +0600

    Add dynamic modal implementation

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const loadAllPlants = async () => {
             cardCont.innerHTML += `
                 <div class="p-4 bg-white rounded-lg">
                     <img class="rounded-lg w-full h-40 overflow-hidden" src="${plant.image}" alt="card image 1">
-                    <h3 class="font-semibold text-[#1F2937] mt-3">${plant.name}</h3>
+                    <h3 class="font-semibold text-[#1F2937] mt-3" onclick='openModal({name: "${plant.name}", image: "${plant.image}", description: "${plant.description}", category: "${plant.category}", price: ${plant.price}})'>${plant.name}</h3>
                     <p class="font-regular text-sm mt-2 line-clamp-3">${plant.description}</p>
                     <div class="flex justify-between items-center mt-3">
                         <a href="#" class="px-3 py-2 bg-[#DCFCE7] font-medium text-sm rounded-3xl">${plant.category}</a>
@@ -71,7 +71,7 @@ const loadPlantByCategories =  async (id) => {
                     <div class="w-full h-40 overflow-hidden rounded-lg">
                         <img class="w-full h-full object-cover" src="${plant.image}" alt="${plant.name}">
                     </div>
-                    <h3 class="font-semibold text-[#1F2937] mt-3">${plant.name}</h3>
+                    <h3 class="font-semibold text-[#1F2937] mt-3" onclick='openModal({name: "${plant.name}", image: "${plant.image}", description: "${plant.description}", category: "${plant.category}", price: ${plant.price}})'>${plant.name}</h3>
                     <p class="text-sm mt-2 line-clamp-3">${plant.description}</p>
                     <div class="flex justify-between items-center mt-3">
                         <a href="#" class="px-3 py-2 bg-[#DCFCE7] font-medium text-sm rounded-3xl">${plant.category}</a>
@@ -109,6 +109,18 @@ const removeSelection = () => {
 // Select the category
 const addSelection = (target) => {
     target.classList.add("bg-[#15803D]", "text-white", "rounded-lg");
+}
+
+// Show modal
+const openModal = (plant) => {
+    document.getElementById("modal-name").innerText = plant.name;
+    document.getElementById("modal-image").src = plant.image;
+    document.getElementById("modal-description").innerText = plant.description;
+    document.getElementById("modal-price").innerText = plant.price;
+    document.getElementById("modal-category").innerText = plant.category;
+
+    const modal = document.getElementById('my_modal_1');
+    modal.showModal();
 }
 
 loadCategories()
