86958e4bf37dc4461878f74080c762e3268bdb7d
Md. Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-09-06 22:55:12 +0600

    Show plants based on categories

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const categoryCont = document.getElementById('category-sidebar');
+const cardCont = document.getElementById('card-container-box');
+
 // Asynchronous function to load categories
 const loadCategories = async () => {
     try {
@@ -5,28 +8,27 @@ const loadCategories = async () => {
         const data = await res.json();
 
         const categories = data.categories;
-        const categoryCont = document.getElementById('category-sidebar');
+
         categories.forEach(cat => {
             categoryCont.innerHTML += `
-                <p class="font-medium mt-1 p-1">${cat.category_name}</p>
+                <p id=${cat.id} class="font-medium mt-1 p-2 cursor-pointer">${cat.category_name}</p>
             `;
         });
+        categoryClick();
     }
     catch (error) {
         console.log(error);
     }
 }
 
-loadCategories()
-
 // Load all tress
-const loadPlants = async () => {
+const loadAllPlants = async () => {
     try {
         const res = await fetch('https://openapi.programming-hero.com/api/plants');
         const data = await res.json();
 
         const plants = data.plants;
-        const cardCont = document.getElementById('card-container-box');
+
         plants.forEach(plant => {
             cardCont.innerHTML += `
                 <div class="p-4 bg-white rounded-lg">
@@ -49,4 +51,65 @@ const loadPlants = async () => {
     }
 }
 
-loadPlants()
+// Load by Categories
+const loadPlantByCategories =  async (id) => {
+    try {
+        if (id === "0") {
+            loadAllPlants();
+            return;
+        }
+
+        const res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`);
+        const data = await res.json();
+
+        const plants = data.plants;
+        cardCont.innerHTML = "";
+
+        plants.forEach(plant => {
+            cardCont.innerHTML += `
+                <div class="p-4 bg-white rounded-lg">
+                    <div class="w-full h-40 overflow-hidden rounded-lg">
+                        <img class="w-full h-full object-cover" src="${plant.image}" alt="${plant.name}">
+                    </div>
+                    <h3 class="font-semibold text-[#1F2937] mt-3">${plant.name}</h3>
+                    <p class="text-sm mt-2 line-clamp-3">${plant.description}</p>
+                    <div class="flex justify-between items-center mt-3">
+                        <a href="#" class="px-3 py-2 bg-[#DCFCE7] font-medium text-sm rounded-3xl">${plant.category}</a>
+                        <p class="font-bold">$ <span>${plant.price}</span></p>
+                    </div>
+                    <button class="rounded-3xl bg-[#15803D] text-white mt-4 w-full py-2 text-sm">Add to Cart</button>
+                </div>
+            `;
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Category click event
+categoryCont.addEventListener('click', (e) => {
+    const target = e.target;
+    const plantId = target.id;
+
+    removeSelection();
+    addSelection(target);
+
+    loadPlantByCategories(plantId);
+
+});
+
+// Remove Category Selection
+const removeSelection = () => {
+    categoryCont.querySelectorAll("p").forEach(p => {
+        p.classList.remove("bg-[#15803D]", "text-white", "rounded-lg");
+    });
+}
+
+// Select the category
+const addSelection = (target) => {
+    target.classList.add("bg-[#15803D]", "text-white", "rounded-lg");
+}
+
+loadCategories()
+loadAllPlants()
